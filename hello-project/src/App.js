a8a7e9a3a5d6ef0cd5e3175a3606da024fc13844
ibrohim1441 <ibrohimkiller7@gmail.com>
2023-03-18 17:15:09 +0500

    first commit

---
+++ hello-project/src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from "./Header/Header";
+import Onesection from "./Onesection/Onesection"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Onesection />
+    </div>
+  );
+}
+
+export default App;
